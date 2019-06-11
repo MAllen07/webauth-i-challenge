f56258b0ff1590a9dccfb2cdf7062fedc27467d5
MAllen07 <mrsmallen07@gmail.com>
2019-06-10 23:03:15 -0400

    created additional folders

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
+server.get("/", (req, res) => {
+    res.send("It's alive!");
+});
+
+//POST REGISTER W/ Hash
+server.post("/api/register", (req, res) => {
+    let user = req.body;
+
+    //hash the password
+    const hash = bcrypt.hashSync(user.password, 8); //password gets re-hashed 2 ^8 times
+    user.password = hash;
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+//POST LOGIN
+server.post("/api/login", (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user) {
+                res.status(200).json({ message: `Welcome ${user.username}!` });
+            } else {
+                res.status(401).json({ message: "You shall not pass!" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+//GET - USERS
+
+server.get("/api/users", (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send({ message: "You shall not pass!" }));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const server = require('./server.js')
 const port = 5000;
 
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-// Libraries
 const express = require('express');
 const helmet = require('helmet');
-const bcrypt = require('bcryptjs');// Add the library
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
+const db = require('./database/dbConfig.js');
+const Users = require('./users/users-model');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send("It's alive!");
+});
